88cc40306b86068cd5141f5594101da525f9199b
Bryan "Beege" Berry <bryanberry235711@gmail.com>
2025-01-28 12:54:23 +0900

    feat: Added db init scripts to seed data

---
+++ database_init/001_tables.sql
@@ -0,0 +1,13 @@
+create table fruits
+(
+    name  text,
+    price integer
+);
+
+create table plantmetadata
+(
+    family    text,
+    genus     text,
+    fruitname text,
+    origin    text
+);
---
+++ database_init/002_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO fruits VALUES 
+    ('apple', '10'),
+    ('mango', '20'),
+    ('grape', '15'),
+    ('durian', '40');
+
+INSERT INTO plantmetadata VALUES 
+    ('Rosaceae', 'Prunus', 'cherry', 'Western Asia'),
+    ('Malvaceae', 'Durio', 'durian', 'Southeast Asia'),
+    ('Musaceae', 'Musa', 'banana', 'Southeast Asia');
